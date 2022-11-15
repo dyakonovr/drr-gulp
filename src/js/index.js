936cfa527d7147fd7ab956322c0be56190300174
dyakonovr <dyakonov.rr@bk.ru>
2022-11-15 16:44:13 +0400

    исправляю gitignore 2.0

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-// Раскоментировать, если нужен .webp
-// import { isWebp } from "./functions/isWebp.js";
-// isWebp();
-
-import "./components/universal/preloader.js";
-import "./components/index/component.js";
