098ac0645ec6d91d36b51c4e050bbc83e315a7cc
dyakonovr <dyakonov.rr@bk.ru>
2022-11-15 16:36:09 +0400

    исправил .gitignore, добавил примеры в index.html

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -12,6 +12,7 @@ export const html = () => {
     }))
     .pipe(typograf({ locale: ['ru', 'en-US'] }))
     .pipe(app.plugins.replace(/@img\//g, 'img/'))
+    .pipe(app.plugins.replace(/@svg\//g, 'img/svg/'))
     .pipe(app.plugins.if(app.isBuild, webpHtmlNosvg()))
     .pipe(
       app.plugins.if(app.isBuild, versionNumber({
